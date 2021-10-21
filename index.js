b101b33ec7c5ac5bd3a22f55298e7847e78aac81
Vivian Gomez <viviangomez0@gmail.com>
2021-10-21 00:10:43 -0400

    commit tree 58ad4056608a126ca906fa34ff91f0fcccc949ea

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+  return`This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog!");
+
+function mondayWork(thing ="go to the office"){
+    return `This Monday, I will ${thing}.`;
+
+}
+
+function wrapAdjective(star="*"){
+   return function(msg="a hard worker"){
+       return `You are ${star}${msg}${star}!`;
+   }
+}
